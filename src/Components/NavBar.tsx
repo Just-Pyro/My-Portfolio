6d1dc7a877d497870b305ef27d3ec81335ad1678
Praise Nacua <praise@email.com>
2025-08-15 12:06:20 +0800

    - Added cat gif - Added background - Added cta button style - Added montserrat font

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="main-navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <div className="logo">
+        <Link to="/">Praise Nacua</Link>
+      </div>
+      <div className="pages">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   );
 };
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,27 +1,42 @@
 import { Link } from "react-router-dom";
+import profile from "../assets/img/portfolio-pic.png";
+import cat from "../assets/gif/cat-programmer.gif";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <>
-      <div className="border">Home test change</div>
-      <div className="headline">
-        <p>
-          Hi, I’m Praise Nacua — I build creative, functional, and clean web
-          solutions.
-        </p>
-      </div>
-      <div className="subheading">
-        <p>
-          I’m a full-stack web programmer who enjoys blending clean code with
-          engaging visuals. From fixing tricky bugs to building complete systems
-          from scratch, I bring ideas to life with a mix of creativity and
-          precision.
-        </p>
-      </div>
-      <div className="call-to-action">
-        <Link to="/projects" className="button">
-          See my Work
-        </Link>
+      <div className="content-wrapper">
+        <div className="headline">
+          <div className="head-left montserrat-regular">
+            <h1>Hi, I’m Praise Nacua</h1>
+            <p> — I build creative, functional, and clean web solutions.</p>
+          </div>
+          <div className="head-right">
+            <div className="img-wrapper">
+              <img src={profile} alt="profile pic" className="profile-img" />
+            </div>
+          </div>
+        </div>
+        <div className="subheading">
+          <div className="sub-left">
+            <img src={cat} alt="Cat Coding" className="cat-gif" />
+          </div>
+          <div className="sub-right montserrat-regular">
+            <p>
+              I’m a full-stack web programmer who enjoys blending clean code
+              with engaging visuals. From fixing tricky bugs to building
+              complete systems from scratch, I bring ideas to life with a mix of
+              creativity and precision.
+            </p>
+          </div>
+        </div>
+        <div className="call-to-action">
+          <Link to="/projects" className="button cta-button">
+            See my Work <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+        </div>
       </div>
     </>
   );
