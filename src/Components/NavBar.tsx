b4493348c6a1ca43570293ce694594227369e9e7
Praise Nacua <praise@email.com>
2025-10-14 13:07:10 +0800

    Added responsive navbar with hamburger menu

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 
 const NavBar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
   return (
     <nav className="main-navbar montserrat-regular">
       <div className="logo">
@@ -9,6 +12,25 @@ const NavBar = () => {
         </HashLink>
       </div>
       <div className="pages">
+        <HashLink smooth to="/#start-nav-reference">
+          Home
+        </HashLink>
+        <HashLink smooth to="/about#start-nav-reference">
+          About
+        </HashLink>
+        <a href="#contact-me">Contact</a>
+      </div>
+
+      <div
+        className={`offscreen-menu-btn ${menuActive ? "active" : ""}`}
+        onClick={() => setMenuActive(!menuActive)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`offscreen-menu ${menuActive ? "active" : ""}`}>
         <HashLink smooth to="/#start-nav-reference">
           Home
         </HashLink>
